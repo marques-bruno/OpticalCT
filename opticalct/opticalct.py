b89e3186d26dcd5d962b569116c25d4e7a3827c2
marques-bruno <bmarques68@gmail.com>
2022-05-01 17:30:24 +0200

    ADD: Projection loader

--- opticalct/opticalct.py
+++ opticalct/opticalct.py
@@ -1,1 +1,29 @@
 """The OpticalCT module."""
+from pathlib import Path
+import cv2
+import numpy as np
+
+class Dataset:
+    projections = None
+
+    def load(self, dir):
+        self.dir = Path(dir)
+        if not self.dir.exists:
+            raise ValueError('directory ' + str(self.dir) + ' does not exist')
+        files = sorted([f for f in self.dir.glob('*.tif')])
+        length = len(files)
+        if length == 0:
+            raise ValueError('directory ' + str(self.dir) + ' does not contain any image')
+
+        cvt2gray = cv2.IMREAD_UNCHANGED
+        sample = cv2.imread(str(files[0]), cv2.IMREAD_UNCHANGED)
+        if len(sample) > 2:
+            cvt2gray = cv2.IMREAD_GRAYSCALE
+
+        print(sample.shape)
+        self.projections = np.empty((length, sample.shape[0], sample.shape[1]), dtype=sample.dtype)
+
+        i = 0
+        for f in files:
+            self.projections[i] = cv2.imread(str(f), cvt2gray)
+            i += 1
--- test/test_opticalct.py
+++ test/test_opticalct.py
@@ -1,8 +1,29 @@
 import unittest
-
-
+from opticalct.opticalct import Dataset
+import numpy as np
+import pytest
+import os
 
 class TestOpticalCT(unittest.TestCase):
 
-    def test_Dataset(self):
+    def test_dataset_load(self):
         data = Dataset()
+        assert data.projections is None
+
+        with pytest.raises(ValueError):
+            data.load('data/non-existentPath')
+
+        with pytest.raises(ValueError):
+            if not os.path.exists('/tmp/pytest_opticalct_emptyFolder'):
+                os.mkdir('/tmp/pytest_opticalct_emptyFolder')
+            data.load('/tmp/pytest_opticalct_emptyFolder')
+
+        data.load('data/small_matrix/projections')
+        assert len(data.projections) == 4
+        assert data.projections.shape == (4, 2, 5)
+        assert data.projections.all() == np.array(
+            [[[0, 48, 102, 101, 0], [0, 48, 102, 101, 0]], 
+            [[64, 54, 75, 42, 16], [64, 54, 75, 42, 16]],
+            [[0, 106, 94, 51, 0], [0, 106, 94, 51, 0]],
+            [[0, 74, 130, 22, 25], [0, 74, 130, 22, 25]]], dtype='uint8').all()
+
