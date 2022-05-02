bc7591ffaed36757df030cf90bff1ae398b0c6b3
marques-bruno <bmarques68@gmail.com>
2022-05-02 12:38:01 +0200

    Dataset load now handles scaling, negatives and other cv2 file formats

--- opticalct/opticalct.py
+++ opticalct/opticalct.py
@@ -2,28 +2,59 @@
 from pathlib import Path
 import cv2
 import numpy as np
+import re
+import logging
+
 
 class Dataset:
     projections = None
+    dir = None
+    format = '*.jpg'
+    scale = 1.0
+    invert = False
 
-    def load(self, dir):
+    def __init__(self, dir=None, **kwargs):
+        if dir is None:
+            return
+        self.load(dir, **kwargs)
+
+    def load(self, dir, **kw):
+        self.format = kw.get('format', self.format)
+        self.scale = kw.get('scale', self.scale)
+        self.invert = kw.get('invert', self.invert)
+
         self.dir = Path(dir)
         if not self.dir.exists:
             raise ValueError('directory ' + str(self.dir) + ' does not exist')
-        files = sorted([f for f in self.dir.glob('*.tif')])
+        files = sorted([f for f in self.dir.glob(self.format)])
         length = len(files)
         if length == 0:
-            raise ValueError('directory ' + str(self.dir) + ' does not contain any image')
+            raise ValueError('directory ' + str(self.dir) +
+                             ' does not contain any image')
 
         cvt2gray = cv2.IMREAD_UNCHANGED
-        sample = cv2.imread(str(files[0]), cv2.IMREAD_UNCHANGED)
+        if self.scale != 1.0:
+            sample = cv2.resize(
+                cv2.imread(str(files[0]), cvt2gray),
+                (0, 0), fx=self.scale, fy=self.scale)
+        else:
+            sample = cv2.imread(str(files[0]), cvt2gray)
         if len(sample) > 2:
             cvt2gray = cv2.IMREAD_GRAYSCALE
 
-        print(sample.shape)
-        self.projections = np.empty((length, sample.shape[0], sample.shape[1]), dtype=sample.dtype)
+        self.projections = np.empty((length, sample.shape[0], sample.shape[1]),
+                                    dtype=sample.dtype)
 
         i = 0
         for f in files:
-            self.projections[i] = cv2.imread(str(f), cvt2gray)
+            logging.warning(str(f))
+            if self.scale != 1.0:
+                self.projections[i] = cv2.resize(
+                    cv2.imread(str(f), cvt2gray),
+                    (0, 0), fx=self.scale, fy=self.scale)
+            else:
+                self.projections[i] = cv2.imread(str(f), cvt2gray)
+            if self.invert:
+                self.projections[i] = 255 - self.projections[i]
             i += 1
+
--- test/test_opticalct.py
+++ test/test_opticalct.py
@@ -6,10 +6,13 @@ import os
 
 class TestOpticalCT(unittest.TestCase):
 
-    def test_dataset_load(self):
+    def test_dataset_ctor(self):
         data = Dataset()
         assert data.projections is None
+        assert data.dir is None
 
+    def test_dataset_load_invalid_input(self):
+        data = Dataset()
         with pytest.raises(ValueError):
             data.load('data/non-existentPath')
 
@@ -18,7 +21,33 @@ class TestOpticalCT(unittest.TestCase):
                 os.mkdir('/tmp/pytest_opticalct_emptyFolder')
             data.load('/tmp/pytest_opticalct_emptyFolder')
 
-        data.load('data/small_matrix/projections')
+    def test_dataset_load_lemon(self):
+        data = Dataset()
+        data.load('data/lemon')
+        assert len(data.projections) == 200
+        assert data.projections.shape == (200, 588, 878)
+
+    def test_dataset_ctor_calls_load(self):
+        data = Dataset()
+        data = Dataset('data/small_matrix/projections', format='*.tif')
+        assert len(data.projections) == 4
+
+    def test_dataset_predefined_arguments(self):
+        data = Dataset()
+        dir = 'data/small_matrix/projections'
+        data.format = '*.tif'
+        data.load(dir)
+        assert len(data.projections) == 4
+
+    def test_dataset_load_scaled(self):
+        data = Dataset()
+        data.load('data/lemon', scale=0.5)
+        assert data.projections.shape == (200, 294, 439)
+
+    def test_dataset_load_small_matrix(self):
+        data = Dataset()
+        dataset_dir = 'data/small_matrix/projections'
+        data.load(dataset_dir, format='*.tif')
         assert len(data.projections) == 4
         assert data.projections.shape == (4, 2, 5)
         assert data.projections.all() == np.array(
@@ -26,4 +55,19 @@ class TestOpticalCT(unittest.TestCase):
             [[64, 54, 75, 42, 16], [64, 54, 75, 42, 16]],
             [[0, 106, 94, 51, 0], [0, 106, 94, 51, 0]],
             [[0, 74, 130, 22, 25], [0, 74, 130, 22, 25]]], dtype='uint8').all()
+        assert data.dir.is_dir() is True
+        assert str(data.dir) == dataset_dir
 
+    def test_dataset_load_small_matrix_invert(self):
+        data = Dataset()
+        dataset_dir = 'data/small_matrix/projections'
+        data.load(dataset_dir, format='*.tif', invert=True)
+        assert data.projections.all() == (255 - np.array(
+            [[[0, 48, 102, 101, 0], [0, 48, 102, 101, 0]], 
+            [[64, 54, 75, 42, 16], [64, 54, 75, 42, 16]],
+            [[0, 106, 94, 51, 0], [0, 106, 94, 51, 0]],
+            [[0, 74, 130, 22, 25], [0, 74, 130, 22, 25]]], dtype='uint8')).all()
+
+    def test_dataset_load_check_frame_order(self):
+        data = Dataset()
+        data.load('data/lemon')
